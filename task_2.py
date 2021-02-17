b92c092fbb2403c7f6819e5dcaa73c117db0f425
antonshe84 <antonshe84@gmail.com>
2021-02-17 22:00:22 +0300

    task 2 edit

--- task_2.py
+++ task_2.py
@@ -4,13 +4,37 @@
 "один"
 # >>> num_translate("eight")
 "восемь"
-Если перевод сделать невозможно, вернуть None. Подумайте, как и где лучше хранить информацию, необходимую для перевода: какой тип данных выбрать, в теле функции или снаружи.
-2. * (вместо задачи 1) Доработать предыдущую функцию num_translate_adv(): реализовать корректную работу с числительными, начинающимися с заглавной буквы. Например:
+Если перевод сделать невозможно, вернуть None. Подумайте, как и где лучше хранить информацию, необходимую для перевода:
+какой тип данных выбрать, в теле функции или снаружи.
+2. * (вместо задачи 1) Доработать предыдущую функцию num_translate_adv(): реализовать корректную работу с числительными,
+ начинающимися с заглавной буквы. Например:
 # >>> >>> num_translate_adv("One")
 "Один"
 # >>> num_translate_adv("two")
 "два"
 """
 
-def num_translate(numb_str):
-    pass
+numb_dict = {   'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
+                'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'
+}
+
+def num_translate_adv(numb_str):
+    if numb_str.istitle():
+        numb_ru = numb_dict.get(numb_str.lower())
+        if numb_ru is None:
+            return None
+        else:
+            return numb_ru.title()
+    else:
+        return numb_dict.get(numb_str)
+
+
+print(num_translate_adv('three'))
+print(num_translate_adv('eleven'))
+print(num_translate_adv('Eight'))
+print(num_translate_adv('Twelve'))
+print(num_translate_adv('Ten'))
+print(num_translate_adv('five'))
+
+
+
